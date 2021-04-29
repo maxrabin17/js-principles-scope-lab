1df50fe7161a65b8d991310b674f81a433077260
maxrabin <max.rabin17@gmail.com>
2021-04-28 20:40:55 -0400

    commit tree 5ef626762b696747d3f753a12f6b6694d5bea684

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = 'bob'
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+    return customerName
+}
+
+function setBestCustomer(){
+    window.bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(overwrite){
+    bestCustomer = overwrite;
+}
+
+const leastFavoriteCustomer = 'something'
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'something else'
+}
+
+changeLeastFavoriteCustomer()
